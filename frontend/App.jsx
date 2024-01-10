dadb8c8afd5fcaa1a4dbc75266a2d8231e78aa29
danielaestefania19 <danielacastellano1000@gmail.com>
2024-01-09 18:21:12 -0600

    Styles Css and Components

--- frontend/App.jsx
+++ frontend/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { Route, Routes, Link, useNavigate } from 'react-router-dom';
 import Home from './components/Home.jsx';
+import Body from './components/Body.jsx';
 import './styles/styles.css'
 import Shop from "./components/Shop.jsx"
 import PaymentDetails from "./components/PaymentDetails.jsx";
 import PaymentButton from "./components/Pay.jsx"
 import { ChakraProvider } from "@chakra-ui/react";
-import Item from "./components/Item.jsx"
 
 function App() {
   const navigate = useNavigate();
@@ -14,29 +14,16 @@ function App() {
   const handleCreatePayment = () => {
     navigate('/other/shop');
   };
-  const seestatus = () => {
-    navigate('pay');
-  };
-  
 
   return (
     <div className="App">
-      <div>
-        <div className="bar">
-          <Link to="/" className="link">Home</Link>
-          <a href="/about" className="link">About</a>
-          <a href="/contact" className="link">Contact</a>
-        </div>
-        <button onClick={handleCreatePayment} className='create-pay'>Ir a la Tienda</button>
-        {/* <button onClick={seestatus} className='create-pay'>Ver detalles</button> */}
-      </div>
       <Routes>
-        <Route path="/" element={<Home />} />
+      <Route path="/" element={<Home/>} />
         <Route path="/other/shop" element={<Shop />} />
         <Route path="/status" element={<PaymentDetails />} />
         <Route path="/pay" element={<PaymentButton />} />
       </Routes>
-
+      <Body onCreatePayment={handleCreatePayment} />
     </div>
   );
 }
