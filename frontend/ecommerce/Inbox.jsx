08f0009e15890fcd2a606cbca8afe6ccde84eebe
ALCAMOjr <alfredocastellanoula.14@gmail.com>
2024-02-17 00:55:05 -0600

    changes in back

--- frontend/ecommerce/Inbox.jsx
+++ frontend/ecommerce/Inbox.jsx
@@ -3,66 +3,102 @@ import { AuthContext } from '../components/AuthContext';
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
+
 const Inbox = () => {
   const [chats, setChats] = useState([]);
   const [selectedChat, setSelectedChat] = useState(null);
   const [privateChat, setPrivateChat] = useState([]);
   const [profiles, setProfiles] = useState({});
-  const { whoami, actor } = useContext(AuthContext); 
+  const { whoami, actor } = useContext(AuthContext);
+  const [messageContent, setMessageContent] = useState("");
 
   useEffect(() => {
-    actor.get_inbox().then((data) => {
-      console.log("Inbox data: ", data);
-      if (data && data.Ok && Array.isArray(data.Ok.conversations)) {
-        const chats = data.Ok.conversations;
-        setChats(chats);
-  
-        // Obtén y guarda los perfiles para todos los chats
-        chats.forEach((chat) => {
-          console.log()
-          actor.get_profile_by_principal(chat.other_user).then((profile) => {
-            console.log(chat.other_user )
-            if (profile && profile.Ok) {
-              setProfiles((prevProfiles) => ({
-                ...prevProfiles,
-                [chat.other_user]: profile.Ok,
-              }));
-            } else {
-              console.error('get_profile_by_principal did not return a profile:', profile);
-            }
+    const intervalId = setInterval(() => {
+      actor.get_inbox().then((data) => {
+        console.log("Inbox data: ", data);
+        if (data && data.Ok && Array.isArray(data.Ok.conversations)) {
+          const chats = data.Ok.conversations;
+          setChats(chats);
+
+          // Obtén y guarda los perfiles para todos los chats
+          chats.forEach((chat) => {
+            actor.get_profile_by_principal(chat.other_user).then((profile) => {
+              console.log(chat.other_user)
+              if (profile && profile.Ok) {
+                setProfiles((prevProfiles) => ({
+                  ...prevProfiles,
+                  [chat.other_user]: profile.Ok,
+                }));
+              } else {
+                console.error('get_profile_by_principal did not return a profile:', profile);
+              }
+            });
           });
+        } else {
+          console.error('get_inbox did not return an array:', data);
+        }
+      });
+    }, 2000); // Actualiza cada 5 segundos
+
+    // Asegúrate de limpiar el intervalo cuando el componente se desmonte
+    return () => clearInterval(intervalId);
+  }, [actor]);
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (selectedChat) {
+        actor.get_private_chat(selectedChat.other_user).then((data) => {
+          if (data && data.Ok && Array.isArray(data.Ok)) {
+            setPrivateChat(data.Ok);
+          } else {
+            console.error('get_private_chat did not return an array:', data);
+          }
         });
+
+        // Llama a get_profile_by_principal con selectedChat.other_user
+        actor.get_profile_by_principal(selectedChat.other_user).then((profile) => {
+          if (profile && profile.Ok) {
+            // Guarda el perfil en el estado
+            setProfiles((prevProfiles) => ({
+              ...prevProfiles,
+              [selectedChat.other_user]: profile.Ok,
+            }));
+          } else {
+            console.error('get_profile_by_principal did not return a profile:', profile);
+          }
+        });
+      }
+    }, 2000); // Actualiza cada 5 segundos
+
+    // Asegúrate de limpiar el intervalo cuando el componente se desmonte
+    return () => clearInterval(intervalId);
+  }, [selectedChat, actor]);
+
+  // Manejador para el evento onChange del campo de entrada
+  const handleInputChange = (event) => {
+    setMessageContent(event.target.value);
+  };
+
+  // Manejador para el evento onClick del botón de enviar
+  const handleSendClick = () => {
+    // Crea el objeto message
+    const message = {
+      content: messageContent,
+      addressee: selectedChat.other_user, // Asegúrate de que selectedChat esté definido
+    };
+
+    // Llama a la función send_message
+    actor.send_message(message).then((result) => {
+      if (result instanceof Error) {
+        console.error('Error al enviar el mensaje:', result);
       } else {
-        console.error('get_inbox did not return an array:', data);
+        console.log('Mensaje enviado con éxito');
+        // Limpia el campo de entrada
+        setMessageContent("");
       }
     });
-  }, [actor]);
-  
-  useEffect(() => {
-    if (selectedChat) {
-      actor.get_private_chat(selectedChat.other_user).then((data) => {
-        if (data && data.Ok && Array.isArray(data.Ok)) {
-          setPrivateChat(data.Ok);
-        } else {
-          console.error('get_private_chat did not return an array:', data);
-        }
-      });
-  
-      // Llama a get_profile_by_principal con selectedChat.other_user
-      actor.get_profile_by_principal(selectedChat.other_user).then((profile) =>  {
-        if (profile && profile.Ok) {
-          // Guarda el perfil en el estado
-          setProfiles((prevProfiles) => ({
-            ...prevProfiles,
-            [selectedChat.other_user]: profile.Ok,
-          }));
-        } else {
-          console.error('get_profile_by_principal did not return a profile:', profile);
-        }
-      });
-    }
-  }, [selectedChat, actor]);
-  
+  };
+
   return (
     <div className="h-screen">
       <div className="container mx-auto">
@@ -128,38 +164,44 @@ const Inbox = () => {
               </div>
 
               <div className="flex items-center justify-between w-full p-3 border-t border-gray-300">
-              <button>
-                <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-gray-500" fill="none" viewBox="0 0 24 24"
-                  stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                </svg>
-              </button>
-              <button>
-                <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-gray-500" fill="none" viewBox="0 0 24 24"
-                  stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13" />
-                </svg>
-              </button>
+                <button>
+                  <svg xmlns="http://www.w3.org/2000/svg" class="w-6 h-6 text-gray-500" fill="none" viewBox="0 0 24 24"
+                    stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                      d="M14.828 14.828a4 4 0 01-5.656 0M9 10h.01M15 10h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+                  </svg>
+                </button>
+                <button>
+                  <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-gray-500" fill="none" viewBox="0 0 24 24"
+                    stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                      d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13" />
+                  </svg>
+                </button>
 
-              <input type="text" placeholder="Message"
-                class="block w-full py-2 pl-4 mx-3 bg-gray-100 rounded-full outline-none focus:text-gray-700"
-                name="message" required />
-              <button>
-                <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-gray-500" fill="none" viewBox="0 0 24 24"
-                  stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                    d="M19 11a7 7 0 01-7 7m0 0a7 7 0 01-7-7m7 7v4m0 0H8m4 0h4m-4-8a3 3 0 01-3-3V5a3 3 0 116 0v6a3 3 0 01-3 3z" />
-                </svg>
-              </button>
-              <button type="submit">
-                <svg class="w-5 h-5 text-gray-500 origin-center transform rotate-90" xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 20 20" fill="currentColor">
-                  <path
-                    d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" />
-                </svg>
-              </button>
+                <input type="text"
+                  placeholder="Message"
+                  className="block w-full py-2 pl-4 mx-3 bg-gray-100 rounded-full outline-none focus:text-gray-700"
+                  name="message"
+                  value={messageContent}
+                  onChange={handleInputChange}
+                  required
+                />
+                <button>
+                  <svg xmlns="http://www.w3.org/2000/svg" class="w-5 h-5 text-gray-500" fill="none" viewBox="0 0 24 24"
+                    stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                      d="M19 11a7 7 0 01-7 7m0 0a7 7 0 01-7-7m7 7v4m0 0H8m4 0h4m-4-8a3 3 0 01-3-3V5a3 3 0 116 0v6a3 3 0 01-3 3z" />
+                  </svg>
+                </button>
+                <button  type="submit" onClick={handleSendClick}>
+                  <svg class="w-5 h-5 text-gray-500 origin-center transform rotate-90" xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20" fill="currentColor">
+                    <path
+                      d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z" />
+                  </svg>
+                </button>
+
                 {/* ... Contenido de los botones y el input de mensaje ... */}
               </div>
             </div>
